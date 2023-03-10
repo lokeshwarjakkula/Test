88bee39d92dcb4e30b3c634da499c258628e5bfb
lokeshwar jakkula <lokeshwarjakkula@gmail.com>
2023-03-10 14:43:41 +0530

    Initial commit

--- src/test/java/lokidemo/Chromebrowser.java
+++ src/test/java/lokidemo/Chromebrowser.java
@@ -2,21 +2,29 @@ package lokidemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Chromebrowser {
-	
-	 public  static   WebDriver OpenChromeBrowser()throws Exception
-	 {
-	
-			 	WebDriver driver; 
-			
-			 	 System.setProperty("webdriver.chrome.driver","C:/chromedriver_win32/chromedriver.exe");
-			 	driver =new ChromeDriver();
-			 	driver.manage().window().maximize() ;	
 
-		    return driver;
-			
+
+
+		WebDriver driver;
+
+		@Test
+
+		public void LoginTest()  {
+
+
+			System.setProperty("webdriver.chrome.driver", "C:/chromedriver_win32/chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://www.google.com/");
+
+			driver.quit();
+
+		}
+
+
 	}
 
 
-}
